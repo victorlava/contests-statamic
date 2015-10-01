ba4672505ae44482af672d42bf2fc638e6f380dd
Victor Lava <l1v1.mate@gmail.com>
2015-10-01 13:39:23 +0300

    final

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include("partials/doc_head.php"); ?>
 <body>
 
-	<section class="section--half text--white" style="background-image:url('img/bg.jpg');">
+	<section id="intro" class="section--half text--white" style="background-image:url('img/bg.jpg');">
 		<div class="section__text text-block--left large-8 large-centered columns object-align--vertical">
 			<p>
 				<img src="img/logo.png" width="87" height="auto">
@@ -9,13 +9,16 @@
 			<h1 class="section__title">Contest Inception!</h1>
 			<p class="font--italic">Your chance to win a baby unicorn!</p> 
 		</div>
+		<div class="button button-enter">
+			<a href="#">Enter</a>
+		</div>
 		<div class="background--overlay" style="background-color:rgba(0,0,0,.70);"></div> 
 	</section>
 
 	<section class="section__header">
 		<div class="row">
 			<div class="large-8 small-centered columns text--center">
-				<p class="header__sub-title"></p>
+				<p class="header__sub-title">Your chance to win a baby unicorn!</p> 
 				<h2 class="header__title">Chance to win!</h2>
 				<p>Unicorn startups, like Instagram (acquired for $1 Billion), WhatsApp (acquired for $19 Billion),  Snapchat (valued at over $5 Billion), AirBnB (valued at over $X Billion), Uber (valued at over $17 Billion), xyz, 123, each did something novel in a new or different way - spotlight is a whole new way to host contests!</p>
 			</div>
@@ -49,6 +52,191 @@
 				</div>
 				<h3 class="title">This this</h3>
 				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, </p>
+			</div>
+		</div>
+	</section>
+ 
+	<section class="section__clients section text--white text--center" style="background-image:url('img/background/clients.jpg');">
+		<div class="row">
+			<h3 class="header__title">Spotlight has hosted contests for company’s like:</h3>
+			<p class="font--italic">If you haven’t already - Go Check out Spotlight for yourself </p>
+		</div> 
+		<div class="row">
+			<div class="large-2 columns">
+				<p><img src="img/clients/1.png" width="" height=""></p>
+			</div>
+			<div class="large-2 columns">
+				<p><img src="img/clients/2.png" width="" height=""></p>
+			</div>
+			<div class="large-2 columns">
+				<p><img src="img/clients/3.png" width="" height=""></p>
+			</div>
+			<div class="large-2 columns">
+				<p><img src="img/clients/4.png" width="" height=""></p>
+			</div>
+			<div class="large-2 columns">
+				<p><img src="img/clients/5.png" width="" height=""></p>
+			</div>
+		</div>
+		<div class="background--overlay" style="background-color:rgba(0,0,0,.30);"></div>
+	</section>
+
+	<section class="section__giveaway section">
+		<div class="row">
+			<div class="large-4 columns">
+				<img src="img/giveaway.jpg" width="370" height="auto">
+			</div>
+			<div class="large-7 columns right">
+				<h3 class="header__title">We love contests SO much</h3>
+				<p>That we’re going to run the ultimate contest - a chance to win the entire spotlight company! Sed ut ullamcorper nulla. Proin ac eleifend dolor. Suspendisse auctor neque et quam bibendum molestie. Fusce facilisis ante elit, et sodales nulla dignissim iaculis. Morbi malesuada quam orci, lacinia laoreet libero congue eu. The winner will receive:</p>
+				<div class="row">
+					<div class="component__icon-list large-4 columns">
+						<div class="icon">
+							<i class="fa fa-facebook"></i>
+						</div>
+						<div class="text">
+							<h5 class="title">CASH BAG</h5>
+							<p>$100,000 cash to help build and grow Spotlight</p>
+						</div>
+						<div class="line"></div>
+					</div>
+					<div class="component__icon-list large-4 columns">
+						<div class="icon">
+							<i class="fa fa-facebook"></i>
+						</div>
+						<div class="text">
+							<h5 class="title">CASH BAG</h5>
+							<p>$100,000 cash to help build and grow Spotlight</p>
+						</div>
+						<div class="line"></div>
+					</div>
+					<div class="component__icon-list large-4 columns">
+						<div class="icon">
+							<i class="fa fa-facebook"></i>
+						</div>
+						<div class="text">
+							<h5 class="title">CASH BAG</h5>
+							<p>$100,000 cash to help build and grow Spotlight</p>
+						</div>
+						<div class="line"></div>
+					</div>
+					<div class="component__icon-list large-4 columns">
+						<div class="icon">
+							<i class="fa fa-facebook"></i>
+						</div>
+						<div class="text">
+							<h5 class="title">CASH BAG</h5>
+							<p>$100,000 cash to help build and grow Spotlight</p>
+						</div>
+
+					</div>
+					<div class="component__icon-list large-4 columns">
+						<div class="icon">
+							<i class="fa fa-facebook"></i>
+						</div>
+						<div class="text">
+							<h5 class="title">CASH BAG</h5>
+							<p>$100,000 cash to help build and grow Spotlight</p>
+						</div>
+					
+					</div>
+					<div class="component__icon-list large-4 columns">
+						<div class="icon">
+							<i class="fa fa-facebook"></i>
+						</div>
+						<div class="text">
+							<h5 class="title">CASH BAG</h5>
+							<p>$100,000 cash to help build and grow Spotlight</p>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="section section--no-padding-bottom">
+		<div class="component__simple-text large-7 columns">
+			<p class="header__sub-title">Your chance to win a baby unicorn!</p> 
+			<h3 class="header__title">About Spotlight</h3>
+			<p>That we’re going to run the ultimate contest - a chance to win the entire spotlight company! Sed ut ullamcorper nulla. Proin ac eleifend dolor. Suspendisse auctor neque et quam bibendum molestie. Fusce facilisis ante elit, et sodales nulla dignissim iaculis. Morbi malesuada quam orci, lacinia laoreet libero congue eu. The winner will receive:</p>
+			<h5 class="title">Important</h5>
+		</div>
+		<div class="large-4 columns">
+			<img src="/img/mobile-spotlight.png" width="400" height="auto">
+		</div>
+	</section>
+
+	<section class="section__email section text--center text--white" style="background-image:url('/img/background/email.jpg')">
+		<div class="row">
+			<h4 class="header__title">Sign Up For Updates on Where To Buy Tickets</h4>
+			<p>Enter your email below to be notified as soon as the contest starts. </br> Don’t miss your chance on half price tickets.</p>
+			<form class="large-4 columns small-centered"> 
+				<div class="large-7 columns">
+					<input type="email" name="email" placeholder="E-mail Address">
+				</div>
+				<div class="large-5 columns">
+					<input type="submit" value="Subscribe" class="button">
+				</div>
+			</form>
+		</div>
+		<div class="background--overlay" style="background-color:rgba(0,0,0,.57);"></div>
+	</section>
+
+	<section class="section section--no-padding-bottom">
+		<div class="large-4 columns">
+			<img src="/img/mobile-spotlight.png" width="400" height="auto">
+		</div>
+		<div class="component__simple-text large-7 columns right">
+			<p class="header__sub-title">Your chance to win a baby unicorn!</p> 
+			<h3 class="header__title">The Contest <span class="smaller">(and How To Win)</span></h3>
+			<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
+			<h5 class="title">Important</h5>
+		</div>
+
+	</section>
+
+	<section class="section">
+		<div class="row">
+			<div class="component__step large-3 columns">
+				<div class="icon">
+					<i class="fa fa-facebook"></i>
+				</div>
+				<div class="text">
+					<h5 class="header__title">01</h5>
+					<h5 class="header__sub-title">Create</h5>
+					<p>Create a Spotlight account under the home page</p>
+				</div>
+			</div>
+			<div class="component__step large-3 columns">
+				<div class="icon">
+					<i class="fa fa-facebook"></i>
+				</div>
+				<div class="text">
+					<h5 class="header__title">01</h5>
+					<h5 class="header__sub-title">Create</h5>
+					<p>Create a Spotlight account under the home page</p>
+				</div>
+			</div>
+			<div class="component__step large-3 columns">
+				<div class="icon">
+					<i class="fa fa-facebook"></i>
+				</div>
+				<div class="text">
+					<h5 class="header__title">01</h5>
+					<h5 class="header__sub-title">Create</h5>
+					<p>Create a Spotlight account under the home page</p>
+				</div>
+			</div>
+			<div class="component__step large-3 columns">
+				<div class="icon">
+					<i class="fa fa-facebook"></i>
+				</div>
+				<div class="text">
+					<h5 class="header__title">01</h5>
+					<h5 class="header__sub-title">Create</h5>
+					<p>Create a Spotlight account under the home page</p>
+				</div>
 			</div>
 		</div>
 	</section>
