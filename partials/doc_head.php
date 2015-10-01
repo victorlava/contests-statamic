b6eaab475d1265d02b8e48af9da824d4b196df30
Victor Lava <l1v1.mate@gmail.com>
2015-10-01 14:46:01 +0300

    fixed section styling

--- partials/doc_head.php
+++ partials/doc_head.php
@@ -7,8 +7,8 @@
 <?php include("meta.php"); ?>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="css/main.css">
-<script src="{{ theme_path }}bower_components/modernizr/modernizr.js"></script>
+<link rel="stylesheet" href="css/main.css"> 
+<script src="/bower_components/jquery/dist/jquery.min.js"></script>
 <!-- feeds -->
 <link rel="alternate" type="application/rss+xml" href="{{ _site_url }}/feed" title="{{ _site_name }} RSS Feed"> 
 <link href='https://fonts.googleapis.com/css?family=Merriweather:400,400italic,700,700italic|Raleway:400,500,600,700,800' rel='stylesheet' type='text/css'>
